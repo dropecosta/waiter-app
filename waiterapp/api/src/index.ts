f454e4bed348718a6e134d4dee72421ae3af832f
Pedro Reis <dropecosta@gmail.com>
2022-12-22 08:56:23 +0000

    Connecting to mongodb

--- waiterapp/api/src/index.ts
+++ waiterapp/api/src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const port = 3002;
-app.listen(port, () => {
-    console.log(`Server listening on localhost http://localhost:${port}`);
-});
+mongoose.connect('mongodb://localhost:27017')
+.then(() => {
+        const app = express();
+        const port = 3002;
+        app.listen(port, () => {
+            console.log(`Server listening on localhost http://localhost:${port}`);
+        });
+
+    })
+    .catch(() => console.log('error connecting to MongoDB'));
+

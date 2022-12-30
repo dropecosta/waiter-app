68f7f11be6102966757c000dcb1b17c27e599664
Pedro Reis <dropecosta@gmail.com>
2022-12-30 10:17:42 +0000

    Adding assets and global styles

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 export default function App() {
-    return <h1>Hello world</h1>;
+    return (
+        <>
+            <GlobalStyles />
+            <h1>Hello world</h1>
+        </>
+    );
 }
